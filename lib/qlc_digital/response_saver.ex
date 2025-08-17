e78287394f830e268ffd1baacbd93f1fbd83d011
kwaw <17128203+amihere@users.noreply.github.com>
2025-08-17 13:35:00 +0000

    move response into note section

--- lib/qlc_digital/response_saver.ex
+++ lib/qlc_digital/response_saver.ex
@@ -24,42 +24,51 @@ defmodule QlcDigital.ResponseSaver do
         :error -> nil
       end
 
+    notes =
+      %{
+        phone_number: conversation.session_id,
+        name: map_correct_value(answers, :start),
+        age: age,
+        location: map_correct_value(answers, :location),
+        country: get_country_value(answers),
+        mental_health_experience:
+          convert_choice_to_text(
+            "mental_health_experience",
+            map_correct_value(answers, :mental_health_experience)
+          ),
+        parenthood_stage:
+          convert_choice_to_text(
+            "parenthood_stage",
+            map_correct_value(answers, :parenthood_stage)
+          ),
+        nursing_concerns:
+          convert_choice_to_text(
+            "nursing_mother_concerns",
+            map_correct_value(answers, :nursing_mother_concerns)
+          ),
+        epds_score: map_correct_value(answers, :epds_score),
+        epds_q1: convert_choice_to_text("epds_q1", map_correct_value(answers, :epds_q1)),
+        epds_q2: convert_choice_to_text("epds_q2", map_correct_value(answers, :epds_q2)),
+        epds_q3: convert_choice_to_text("epds_q3", map_correct_value(answers, :epds_q3)),
+        epds_q4: convert_choice_to_text("epds_q4", map_correct_value(answers, :epds_q4)),
+        epds_q5: convert_choice_to_text("epds_q5", map_correct_value(answers, :epds_q5)),
+        epds_q6: convert_choice_to_text("epds_q6", map_correct_value(answers, :epds_q6)),
+        epds_q7: convert_choice_to_text("epds_q7", map_correct_value(answers, :epds_q7)),
+        epds_q8: convert_choice_to_text("epds_q8", map_correct_value(answers, :epds_q8)),
+        epds_q9: convert_choice_to_text("epds_q9", map_correct_value(answers, :epds_q9)),
+        epds_q10: convert_choice_to_text("epds_q10", map_correct_value(answers, :epds_q10)),
+        feedback: map_correct_value(answers, :feedback_request),
+        checkout_choice:
+          convert_choice_to_text("checkout_point", map_correct_value(answers, :checkout_point)),
+        notes: "From the Whatsapp Bot"
+      }
+      |> Enum.filter(fn {_key, value} -> value != nil end)
+      |> Enum.into(%{})
+
     %{
       phone_number: conversation.session_id,
-      name: map_correct_value(answers, :start),
-      age: age,
-      location: map_correct_value(answers, :location),
-      country: get_country_value(answers),
-      mental_health_experience:
-        convert_choice_to_text(
-          "mental_health_experience",
-          map_correct_value(answers, :mental_health_experience)
-        ),
-      parenthood_stage:
-        convert_choice_to_text("parenthood_stage", map_correct_value(answers, :parenthood_stage)),
-      nursing_concerns:
-        convert_choice_to_text(
-          "nursing_mother_concerns",
-          map_correct_value(answers, :nursing_mother_concerns)
-        ),
-      epds_score: map_correct_value(answers, :epds_score),
-      epds_q1: convert_choice_to_text("epds_q1", map_correct_value(answers, :epds_q1)),
-      epds_q2: convert_choice_to_text("epds_q2", map_correct_value(answers, :epds_q2)),
-      epds_q3: convert_choice_to_text("epds_q3", map_correct_value(answers, :epds_q3)),
-      epds_q4: convert_choice_to_text("epds_q4", map_correct_value(answers, :epds_q4)),
-      epds_q5: convert_choice_to_text("epds_q5", map_correct_value(answers, :epds_q5)),
-      epds_q6: convert_choice_to_text("epds_q6", map_correct_value(answers, :epds_q6)),
-      epds_q7: convert_choice_to_text("epds_q7", map_correct_value(answers, :epds_q7)),
-      epds_q8: convert_choice_to_text("epds_q8", map_correct_value(answers, :epds_q8)),
-      epds_q9: convert_choice_to_text("epds_q9", map_correct_value(answers, :epds_q9)),
-      epds_q10: convert_choice_to_text("epds_q10", map_correct_value(answers, :epds_q10)),
-      feedback: map_correct_value(answers, :feedback_request),
-      checkout_choice:
-        convert_choice_to_text("checkout_point", map_correct_value(answers, :checkout_point)),
-      notes: "From the Whatsapp Bot"
+      notes: notes
     }
-    |> Enum.filter(fn {_key, value} -> value != nil end)
-    |> Enum.into(%{})
   end
 
   defp get_country_value(answers) do
