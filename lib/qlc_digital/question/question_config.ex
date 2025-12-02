8af1f5afe463d83a048fdbb5f50877d8bb123910
kwaw <17128203+amihere@users.noreply.github.com>
2025-12-02 08:46:36 +0000

    undo replace

--- lib/qlc_digital/question/question_config.ex
+++ lib/qlc_digital/question/question_config.ex
@@ -155,8 +155,9 @@ defmodule QlcDigital.Question.QuestionConfig do
           options =
             String.split(options_str, ",")
             |> Enum.map(&String.trim/1)
-            # used to include commas for yes, no preceding
-            |> String.replace(~r/\b(Yes|No)\b/, "\\1,")
+
+          # used to include commas for yes, no preceding
+          # |> String.replace(~r/\b(Yes|No)\b/, "\\1,")
 
           Map.put(acc, :options, options)
 
